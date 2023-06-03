8fb8f417b583bcb3ef9b4fd5e00d50e202d0edc2
VolodymyrSkurikhin <vladiskuri@gmail.com>
2023-06-03 23:58:45 +0300

    fix border radius

--- softycat/src/components/StyledCardContainer.styled.tsx
+++ softycat/src/components/StyledCardContainer.styled.tsx
@@ -6,5 +6,6 @@ export const StyledCardContainer = styled('div')({
   flexDirection: "column",
   justifyContent: "space-between",
   backgroundColor: "white",
-  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  borderRadius: "8px",
 })
--- softycat/src/components/StyledImgContainer.styled.tsx
+++ softycat/src/components/StyledImgContainer.styled.tsx
@@ -4,5 +4,6 @@ export const StyledImgContainer = styled('div')({
   display: "flex",
   // width: "100",
   // position: "relative",
-  flex: "2 0 100%"
+  flex: "2 0 100%",
+  borderRadius: "8px",
 })
