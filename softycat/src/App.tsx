bf59686c54b46784e41dea5e2f8bb3870c71971c
VolodymyrSkurikhin <vladiskuri@gmail.com>
2023-06-11 22:16:58 +0300

    fix navlink active style

--- softycat/src/App.tsx
+++ softycat/src/App.tsx
@@ -1,12 +1,22 @@
-import OwnerList from "./components/OwnerList";
+import OwnerList from "./components/cards/OwnerList";
 import { Container } from "./App.styled";
+import { CommonContainer, Header, Nav, Logo, Link } from "./components/common/Common.styled";
 import list from "./Service/list.json";
 
 const App: React.FC = () => {
-  return (
+  return (<CommonContainer>
+    <Header>
+      <Logo>Test</Logo>
+      <Nav>
+        <Link to="/about">About
+        </Link>
+        <Link to="/cats">Cats</Link>
+      </Nav>
+    </Header>
     <Container>
       <OwnerList owners={list} />
     </Container>
+  </CommonContainer>
   );
 }
 
---
+++ softycat/src/components/common/Common.styled.tsx
@@ -0,0 +1,101 @@
+import styled from "@emotion/styled";
+// import { hover } from "@testing-library/user-event/dist/hover";
+// import { FC } from 'react'
+import { NavLink } from "react-router-dom";
+
+export const CommonContainer = styled('div')({
+  maxWidth: "1200px",
+  margin: " 0 auto",
+  padding: "0 16px"
+})
+
+export const Header = styled('header')({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space - between",
+  gap: "12px",
+  padding: "8px 0",
+  marginBottom: "16px",
+  borderBottom: "1px solid black"
+})
+
+export const Link = styled(NavLink)({
+  padding: "8px 16px",
+  borderRadius: "4px",
+  textDecoration: "none",
+  color: "black",
+  fontWeight: "500",
+  '&.active': { backgroundColor: "orangered" }
+}
+)
+
+export const Nav = styled('nav')({
+  display: "flex"
+})
+
+
+export const Logo = styled('p')({
+  fontWeight: "700",
+  margin: "0"
+})
+
+// interface ComponentProps {
+//   to: string,
+//   className?: string,
+//   children: any
+// }
+
+// const Navig: FC<ComponentProps> = ({ to, children }) => (
+//   <NavLink to={to} className={({ isActive, isPending }) =>
+//     isPending ? "pending" : isActive ? "active" : ""
+//   } >{children}</NavLink>
+// )
+
+
+
+
+  // &.active{ backgroundColor: "orangered" })
+
+  //   backgroundColor: `${props}=>{props.className={({ isActive }) => isActive ? "active" : ""};
+  //   ${props.className}==="active"?"white":"green"`
+  //   // props => {
+  //   //   props.className === "active" ? {
+  //   //     color: "white",
+  //   //     backgroundColor: "orangered"
+  //   //   } : ""
+  // })
+
+
+  // props => {
+  //   console.log(props);
+  //   if (props.className === "active") return {
+  //     color: "white",
+  //     backgroundColor: "orangered"
+  //   }
+  // })
+
+
+// export function Navbar(props: React.AllHTMLAttributes<HTMLElement>) {
+//   return (
+//     <NavbarDiv>
+//       <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
+//         Home
+//       </NavLink>
+//       <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
+//         About
+//       </NavLink>
+//     </NavbarDiv>
+//   )
+// }
+
+// const NavbarDiv = styled('div')({
+//   width: "100 %",
+//   height: "85px",
+//   backgroundColor: "#242424",
+//   color: "blue",
+//   boxShadow: "0 2px 4px 0 rgba(0, 0, 0, .25)"
+// })
+
+
+
+
